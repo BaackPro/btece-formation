94d2fe4bd245f56b76811e7f512d1508332d4d37
BaackPro <btece08@gmail.com>
2025-08-20 19:51:26 +0100

    Auto-update 20/08/2025 19:51

--- push.ps1
+++ push.ps1
@@ -5,3 +5,11 @@ git commit -m "Auto-update $(Get-Date -Format 'dd/MM/yyyy HH:mm')"
 git push origin main
 
 Santanda
+
+
+
+
+cd "C:\Users\prince CHABI\Desktop\btece"
+git add --all
+git commit -m "Auto-update $(Get-Date -Format 'dd/MM/yyyy HH:mm')"
+git push origin main
