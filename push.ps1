c57ca87a595a55a8c4f40780c8b17f4986034f14
BaackPro <btece08@gmail.com>
2025-08-31 13:28:26 +0100

    Auto-update 31/08/2025 13:28

--- push.ps1
+++ push.ps1
@@ -4,8 +4,22 @@ git add --all
 git commit -m "Auto-update $(Get-Date -Format 'dd/MM/yyyy HH:mm')"
 git push origin main
 
+
+git push origin HEAD
+
 Santanda
 
+
+
+cd "C:\Users\prince CHABI\Desktop\btece"
+# D'abord récupérer les changements distants
+git pull origin main
+# Ensuite ajouter vos modifications
+git add --all
+# Créer un commit
+git commit -m "Auto-update $(Get-Date -Format 'dd/MM/yyyy HH:mm')"
+# Finalement pousser vers GitHub
+git push origin main
 
 
 
